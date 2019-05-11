50c09aa29e163edfe211acb4c1a85ea197118255
eda3 <gerorin1010@gmail.com>
2019-05-12 03:38:43 +0900

    rebuilding site 2019年 5月12日 日曜日 03時38分43秒 JST

---
+++ deploy.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+echo -e "\033[0;33mDeploying updates to GitHub...\033[0m"
+
+# Build the project.
+hugo
+
+# Go to public folder
+cd public
+
+# Add changes to git.
+git add -A
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+
+# Come back
+cd ..
+
+# Add changes to git.
+git add -A
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
