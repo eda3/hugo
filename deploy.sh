6813d353ee9dc5661a4419b31b34a6e2ee59f6a2
eda3 <gerorin1010@gmail.com>
2024-01-05 07:24:36 +0000

    コメント追加

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,14 @@
 echo -e "\033 [0;33mDeploying updates to GitHub...\033 [0m"
 
 # Build the project.
+echo "###### hugo ####"
 hugo
 
 # Go to public folder
 cd public
 
 # Add changes to git.
+echo "###### git add -A ####"
 git add -A
 
 # Commit changes.
@@ -16,15 +18,18 @@ msg="rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
+echo "###### git commit ####"
 git commit -m "$msg"
 
 # Push source and build repos.
+echo "###### git push origin master ####"
 git push origin master
 
 # Come back
 cd ..
 
 # Add changes to git.
+echo "###### git push origin master ####"
 git add -A
 
 # Commit changes.
@@ -32,7 +37,9 @@ msg="rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
+echo "###### git commit -m ####"
 git commit -m "$msg"
 
 # Push source and build repos.
+echo "###### git push origin master ####"
 git push origin master
